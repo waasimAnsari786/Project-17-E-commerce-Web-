833a4f6b712ee1118da47dafb936bd103383f793
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-24 13:13:44 -0700

    Update index.html , style.css and app.js
    I created the header.

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ let navbarParent = document.querySelector(".my-nav");
 let hiddenNavDiv = document.querySelector(".my-nav-inner div");
 let count = true;
 
-navbarParent.addEventListener("click" , (evt) => {
-    if (evt.target.classList.contains("fa-solid")) {
-        if (count) {
+const navBarShowHide = () => {
+    let headerCtnr = document.querySelector(".header-ctnr");
+    if (count) {
             hiddenNavDiv.style.transform = 'scale(1)';
+            headerCtnr.style.marginTop = '20rem';
             count = false;
         }
 
         else{
             hiddenNavDiv.style.transform = 'scale(0)';
+            headerCtnr.style.marginTop = '0rem';
             count = true;
         }
+};
+
+navbarParent.addEventListener("click" , (evt) => {
+    if (evt.target.classList.contains("fa-solid")) {
+        navBarShowHide();
     }
 }); 
