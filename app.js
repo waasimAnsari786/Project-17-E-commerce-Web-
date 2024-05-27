f5bef702221a1f7ece6d72f15dacfa7991fc881c
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-27 03:49:22 -0700

    Update index.html , style.css and all files of JavaScript.
    I made some changes in the function choosing the quantity of the product and i also started the add to cart button function.

--- app.js
+++ app.js
@@ -3,28 +3,34 @@ import { navBarShowHide } from "./functions.js";
 
 import { navbarParent, productStock } from "./variables.js";
 
-navbarParent.addEventListener("click" , (evt) => {
+navbarParent.addEventListener("click", (evt) => {
     if (evt.target.classList.contains("fa-solid")) {
         navBarShowHide();
     }
-}); 
+});
 
 // navbar's code end
 
 // products section's code
 import { productMainDiv } from "./variables.js";
 import { chooseQuantityOfProduct } from "./functions.js";
-// import { chooseQuantityOfProduct2 } from "./functions.js";
+import { addToCartBtnFunc } from "./functions.js";
 
-let couter = 1;
 productMainDiv.addEventListener("click", (evt) => {
-    console.log(evt.target);
     if (evt.target.classList.contains("plus-btn")) {
-        chooseQuantityOfProduct(true);
-    } else if (evt.target.classList.contains("minus-btn")) {
-        chooseQuantityOfProduct(false);
+        let updatedElem = evt.target.parentElement.nextElementSibling.firstElementChild;
+        let stock = evt.target.closest(".product-mb").querySelector(".product-stock").innerText.split(" ");
+        chooseQuantityOfProduct(true, updatedElem, stock[stock.length - 1]);
+    }
+
+    else if (evt.target.classList.contains("minus-btn")) {
+        let updatedElem = evt.target.parentElement.previousElementSibling.firstElementChild;
+        chooseQuantityOfProduct(false, updatedElem, undefined);
+    }
+
+    else if (evt.target.classList.contains("add-to-cart-btn")) {
+        addToCartBtnFunc();
     }
 });
-
 
 // products section's code end
--- functions.js
+++ functions.js
@@ -4,34 +4,35 @@ import { hiddenNavDiv, productStock } from "./variables.js";
 import { countObj } from "./objects.js";
 
 export const navBarShowHide = () => {
-    let headerCtnr = document.querySelector(".header-ctnr");
-    if (countObj.count) {
-        setTimeout(() => {
-            hiddenNavDiv.style.transform = 'scale(1)';
-        }, 100);
-        headerCtnr.style.marginTop = '20rem';
-        countObj.count = false;
-    }
+  let headerCtnr = document.querySelector(".header-ctnr");
+  if (countObj.count) {
+    setTimeout(() => {
+      hiddenNavDiv.style.transform = 'scale(1)';
+    }, 100);
+    headerCtnr.style.marginTop = '20rem';
+    countObj.count = false;
+  }
 
-    else {
-        hiddenNavDiv.style.transform = 'scale(0)';
-        setTimeout(() => {
-            headerCtnr.style.marginTop = '0rem';
-        }, 50);
-        countObj.count = true;
-    }
+  else {
+    hiddenNavDiv.style.transform = 'scale(0)';
+    setTimeout(() => {
+      headerCtnr.style.marginTop = '0rem';
+    }, 50);
+    countObj.count = true;
+  }
 };
 
 // navbar's code end
 
 // products section's code
-import { productMinusBtn, productNumBtn, productPlusBtn } from "./variables.js";
+import { productMinusBtn , productNumBtn , productPlusBtn } from "./variables.js";
 
-export const chooseQuantityOfProduct = (increment) => {
+export const chooseQuantityOfProduct = (increment, updatedElem, stock) => {
+  countObj.count = updatedElem.innerText;
   if (increment) {
     countObj.count++;
-    if (countObj.count > productStock.innerText) {
-      countObj.count = productStock.innerText;
+    if (countObj.count > stock) {
+      countObj.count = stock;
     }
   } else {
     countObj.count--;
@@ -39,6 +40,19 @@ export const chooseQuantityOfProduct = (increment) => {
       countObj.count = 1;
     }
   }
-  productNumBtn.innerText = countObj.count;
+  return updatedElem.innerText = countObj.count;
+};
+
+export const addToCartBtnFunc = () => {
+  let productAddedNotificationdiv = document.createElement("div");
+  productAddedNotificationdiv.classList.add("product-added-notification-div");
+  productAddedNotificationdiv.innerText = 'Product ID 1 has been added'
+  document.querySelector("body").prepend(productAddedNotificationdiv);
+
+  productAddedNotificationdiv.style.position = 'fixed';
+  productAddedNotificationdiv.style.transform = 'translateX(0)';
+  productAddedNotificationdiv.style.transition = '0.5s';
+
+  return productAddedNotificationdiv;
 };
 // products section's code end
